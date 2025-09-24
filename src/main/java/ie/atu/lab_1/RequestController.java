4dfef5539f9b7d10327699fa46ec876c51b7101e
G00421777@atu.ie <G00421777@atu.ie>
2025-09-24 15:00:28 +0100

    Added method greetName

--- src/main/java/ie/atu/lab_1/RequestController.java
+++ src/main/java/ie/atu/lab_1/RequestController.java
@@ -1,6 +1,8 @@
 package ie.atu.lab_1;
 
+import org.springframework.boot.context.properties.bind.Name;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,8 @@ public class RequestController {
     public String hello(){
         return "hello";
     }
-
+@GetMapping ("/Greet/{name}")
+    public String greetName(@PathVariable String name) {
+        return "HELLO " + name + "!";
+}
 }
